a1b2bce93140afaa60be80fd8c262f222547fc88
William neves <carcaju@carcaju.com.br>
2018-01-30 11:49:37 -0200

    PageAdmin

--- index.php
+++ index.php
@@ -4,18 +4,22 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \carcaju\Page;
+use \carcaju\PageAdmin;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
     $page = new Page();
-
     $page->setTpl("index");
-	
 });
+
+$app->get('/admin', function() {
+    $page = new PageAdmin();
+    $page->setTpl("index");
+});
+
 
 $app->run();
 
