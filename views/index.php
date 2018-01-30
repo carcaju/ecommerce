c77afeaa45cf53610128983e0f59086d462c88c1
William neves <carcaju@carcaju.com.br>
2018-01-30 11:08:17 -0200

    Page

--- views/index.php
+++ views/index.php
@@ -2,19 +2,16 @@
 
 require_once("vendor/autoload.php");
 
-use \Slim\Slim;
-use \carcaju\Page;
-
-$app = new Slim();
+$app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-    $page = new Page();
+	$page = new Hcode\Page();
 
-    $page->setTpl("index");
-	
+	$page->setTpl("index");
+
 });
 
 $app->run();
