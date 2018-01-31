d82abdba9dcdbb3891b3d77187636274858ec982
William neves <carcaju@carcaju.com.br>
2018-01-30 22:29:36 -0200

    tela Login

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \carcaju\Page;
 use \carcaju\PageAdmin;
+use \carcaju\Model\User;
 
 $app = new Slim();
 
@@ -16,10 +17,39 @@ $app->get('/', function() {
 });
 
 $app->get('/admin', function() {
+    
+	User::verifyLogin();
+
     $page = new PageAdmin();
+
     $page->setTpl("index");
 });
 
+
+$app->get('/admin/login', function() {
+    $page = new PageAdmin([
+    	"header"=>false,
+    	"footer"=>false
+    ]);
+    $page->setTpl("login");
+});
+
+
+$app->post('/admin/login', function() {
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+
+	exit;
+});
+
+
+$app->get('/admin/logout',function() {
+
+	User::logout();
+	header("Location: /admin/login");
+	exit;
+});
 
 $app->run();
 
