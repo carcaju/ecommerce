aba5c2ccc65885d119ebf67c2379e5fa11e89fec
William neves <carcaju@carcaju.com.br>
2018-02-13 18:30:46 -0200

    categorias vs produtos

---
+++ site.php
@@ -0,0 +1,32 @@
+<?php
+
+use \carcaju\Page;
+use \carcaju\Model\Product;
+use \carcaju\Model\Category;
+
+$app->get('/', function() {
+	$products = Product::listAll();
+    $page = new Page();
+
+    $page->setTpl("index",[
+    	'products'=>Product::checkList($products)
+    ]);
+});
+
+$app->get("/categories/:idcategory",function($idcategory) {
+
+	$category = new Category;
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category",[
+		'category'=>$category->getValues(),
+		'products'=>Product::checkList($category->getProducts())
+	]);
+
+
+});
+
+?>
